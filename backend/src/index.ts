f1b82a9f9d377d8866345775419d6cf777dec753
Abhay Mishra <abhaym1205@gmail.com>
2025-01-15 11:48:07 +0530

    added Google tag Manager and Analytics

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,13 +12,37 @@ const app: Application = express();
 const PORT = process.env.PORT || 4000;
 
 // * Middleware
-app.use(
-  cors({
-    origin: [ENV.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: [
+    "Authorization",
+    "Content-Type",
+    "Accept",
+    "Cache-Control",
+    "DNT",
+    "If-Modified-Since",
+    "Keep-Alive",
+    "Origin",
+    "User-Agent",
+    "X-Requested-With",
+    "company-code",
+  ],
+  exposedHeaders: ["Content-Length", "Content-Range"],
+  optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+// app.use(
+//   cors({
+//     origin: [ENV.FRONTEND_URL, "https://basepackers.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morganMiddleware);
